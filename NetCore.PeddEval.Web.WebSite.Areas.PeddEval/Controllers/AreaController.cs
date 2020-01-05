70777ea80bdb757f98753bfed3181532d570d8ed
Ro <rodolfo.quezada.s@gmail.com>
2020-01-05 18:22:13 -0300

    subida dev
    creacion de vista de mantenedores

---
+++ NetCore.PeddEval.Web.WebSite.Areas.PeddEval/Controllers/AreaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Data;
+
+namespace NetCore.PeddEval.Web.WebSite.Areas.PeddEval.Controllers
+{
+
+    [Authorize]
+    public class AreaController : CoreLibrary.Web.Base.Controller
+    {
+
+        [Authorize]
+        public ActionResult Index()
+        {
+
+            ViewBag.Area = null;
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //se carga el viewbag de los Estados
+
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
